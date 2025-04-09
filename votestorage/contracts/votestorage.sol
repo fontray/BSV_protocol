f281b6b08e27b9da5ba3ffc3aa914d80b9858e90
fontray <100141401+fontray@users.noreply.github.com>
2025-04-09 21:34:15 +0800

    Update votestorage.sol

--- votestorage/contracts/votestorage.sol
+++ votestorage/contracts/votestorage.sol
@@ -9,17 +9,17 @@ contract VoteStorage {
 
     Vote[] private votes;
 
-    /// @dev 存储选票
+    /// @dev store votes
     function storeVote(bytes memory _signature, bytes memory _randomNumber) public {
         votes.push(Vote(_signature, _randomNumber));
     }
 
-    /// @dev 获取选票总数
+    /// @dev get votes total number
     function getVotesCount() public view returns (uint256) {
         return votes.length;
     }
 
-    /// @dev 根据索引获取选票信息
+    /// @dev get vote data
     function getVote(uint256 index) public view returns (bytes memory, bytes memory) {
         require(index < votes.length, "Index out of range");
         Vote memory vote = votes[index];
